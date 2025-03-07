750f1feec1df59614e76d499014ab1bf84048626
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-03-07 08:18:58 +0800

    Fix style: text component

--- src/app/_components/Text.tsx
+++ src/app/_components/Text.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from '@/app/page.module.css'
 
 type Props = {
   content: string
 }
 const Text: React.FC<Props> = ({ content }) => {
-  return <div dangerouslySetInnerHTML={{__html: content}}/>
+  return <div className={classes.homeBoxText} dangerouslySetInnerHTML={{__html: content}}/>
 };
 
 export default Text;
