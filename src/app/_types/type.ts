a8d0b70c4a9df3f47a5e7400a96987ca114599e2
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-03-07 08:12:02 +0800

    Fix: type

--- src/app/_types/type.ts
+++ src/app/_types/type.ts
@@ -1,15 +1,75 @@
-export type MicroCmsPost = {
-  id: string,
+export type PostData = {
+  id: number,
   title: string,
   content: string,
+  thumbnailUrl: string,
   createdAt: string,
-  categories: { id: string; name: string }[],
-  thumbnail: { url: string; height: number; width: number }
+  updatedAt: string,
+  postCategories: {
+    category: {
+      id: number,
+      name: string,
+    }
+  }[],
 };
 
-export type MicroCmsCategory = {
-  categories: { 
-    id: string,
-    name: string
-  }[]
+export type CreatePostRequestBody = {
+  title: string;
+  content: string;
+  categories: { id: number; name: string }[];
+  thumbnailUrl: string;
 };
+
+export type CategoryData = {
+  id: number,
+  name: string,
+  createdAt: string,
+  updatedAt: string,
+}
+
+export type CreateCategoryRequestBody = {
+  name: string,
+};
+
+export type SelectOptionForCategories = {
+  value: number,
+  label: string,
+}
+
+export type SelectedCategoryData = {
+  category: {
+    id: number,
+    name: string,
+  }
+}
+ 
+// Front-end Category Button
+export type Category = {
+  id: number,
+  name: string,
+}
+
+export type CategoriesButtonProps = {
+  postCategories: {
+    category: {
+      id: number,
+      name: string,
+    }
+  }[]
+}
+
+// export type MicroCmsPost = {
+//   id: string,
+//   title: string,
+//   content: string,
+//   createdAt: string,
+//   categories: { id: string; name: string }[],
+//   thumbnail: { url: string; height: number; width: number }
+// };
+
+// export type MicroCmsCategory = {
+//   categories: { 
+//     id: string,
+//     name: string
+//   }[]
+// };
