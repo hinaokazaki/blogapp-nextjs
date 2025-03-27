5fa14b7d3902170968a1920243598eac0f3b5d98
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-03-27 13:45:56 +0800

    add login, signout link to Header

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,46 @@
+'use client' 
+import Link from "next/link"
+import React from "react"
+import { useSupabaseSession } from "../_hooks/useSupabaseSession"
+import { supabase } from "@/utils/supabase"
+import { useRouter } from "next/navigation"
+
+export const Header: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    router.replace('/') // `window.location.href` の代わり
+  }
+
+  const { session, isLoading } = useSupabaseSession()
+
+  return (
+    <header className="nav">
+      <Link href='/' className='headerLink'>
+        Blog
+      </Link>
+      {!isLoading && (
+        <div>
+          {session ? (
+            <>
+              <Link href='/admin' className='headerLink'>
+                管理画面
+              </Link>
+              <button onClick={handleLogout}>ログアウト</button>
+            </>
+          ) : (
+            <>
+              <Link href='/contact' className='headerLink'>
+                お問い合わせ
+              </Link>
+              <Link href='/login' className='headerLink'>
+                ログイン
+              </Link>
+            </>
+          )}
+        </div>
+      )}
+    </header>
+  )
+}
