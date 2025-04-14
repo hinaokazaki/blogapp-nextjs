d2828dd6b9b1540419428bb2b7209c9717ca0e0f
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-04-14 23:49:56 +0800

    add css

--- src/app/_components/Categories.tsx
+++ src/app/_components/Categories.tsx
@@ -5,7 +5,7 @@ const Categories: React.FC<CategoriesButtonProps> = ({ postCategories }) => {
   return (
     <div>
       {postCategories.map((item, index) => 
-        <button key={index} type='button' className='text-[#06c] border-[1px] border-solid border-[#06c] rounded-sm p-2 bg-white'>{item.category.name}</button>
+        <button key={index} type='button' className='text-[#06c] border-[1px] border-solid border-[#06c] rounded-sm p-2 ml-1 bg-white'>{item.category.name}</button>
       )}
     </div>
   )
